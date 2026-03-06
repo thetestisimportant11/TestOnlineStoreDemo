9ef3d8f9321736bef64b7006e6251df1f52ed54e
evgenyqa <thetestisimportant@gmail.com>
2026-03-06 12:45:27 +0500

    Изменение теста Title

--- src/test/java/TestTitle.java
+++ src/test/java/TestTitle.java
@@ -1,34 +1,16 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestTitle {
-    public WebDriver driver;
-
-    @BeforeEach
-    public void SetUp () {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-    }
+public class TestTitle extends BazaTesta{
 
     @Test
-    public void checkTitle () throws InterruptedException {
+    public void checkTitle () {
         driver.get("https://mega.readyscript.ru/");
-        Thread.sleep(2000);
-        String expectedResult = "ReadyScript Гипермаркет - демонстрационный интернет магазин";
+
+        String expectedResult = ("ReadyScript Гипермаркет - демонстрационный интернет магазин");
         String actualResult = driver.getTitle();
+
         assertEquals(expectedResult, actualResult, "Название не соответствует ожиданию!");
     }
-
-    @AfterEach
-    public void tearDown (){
-        driver.quit();
-    }
-
 }
