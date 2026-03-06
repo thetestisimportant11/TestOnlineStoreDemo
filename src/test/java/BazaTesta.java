8ac929d55dd05a6d02e5a7bd54716385f882b2c0
evgenyqa <thetestisimportant@gmail.com>
2026-03-06 15:01:26 +0500

    Добавление в проект файлов об автоматизированном тестировании

--- src/test/java/BazaTesta.java
+++ src/test/java/BazaTesta.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 
 abstract class BazaTesta {
